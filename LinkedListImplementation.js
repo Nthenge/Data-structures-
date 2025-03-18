fd86b98b22022b8ada9bde6af9670545bbb73cc8
Nthenge <abrahamnetsec@gmail.com>
2025-03-18 14:39:06 +0300

    Added linked list implementation code in js

---
+++ LinkedListImplementation.js
@@ -0,0 +1,24 @@
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+const node1 = new Node(3);
+const node2 = new Node(5);
+const node3 = new Node(13);
+const node4 = new Node(2);
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+let currentNode = node1
+
+while(currentNode){
+    process.stdout.write(currentNode.data + " -> ");
+    currentNode = currentNode.next;
+}
+
+console.log('null')
