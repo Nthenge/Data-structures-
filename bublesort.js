4ce90533e8be645b69c60101879985c690482bd3
Nthenge <abrahamnetsec@gmail.com>
2025-04-11 15:32:11 +0300

    Modified bublesort

--- bublesort.js
+++ bublesort.js
@@ -5,9 +5,10 @@ for(let i = 0; i < n - 1; i++){
     let swapped = false
 
     for(let j = 0; j < n - i - 1; j++){
-        if(arr[j] > arr[j + 1])
+        if(arr[j] > arr[j + 1]){
             [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
             swapped = true
+        }
     }
 
     if(!swapped)
